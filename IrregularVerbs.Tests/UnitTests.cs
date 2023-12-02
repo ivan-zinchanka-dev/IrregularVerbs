b56ed72fcc651c2b83b5ec79d672c4b45fa3aa13
JanZinch <jan.zinchanka@mail.ru>
2023-12-03 00:23:21 +0300

    setup: unit tests

---
+++ IrregularVerbs.Tests/UnitTests.cs
@@ -0,0 +1,20 @@
+using IrregularVerbs.Models.Verbs.Components;
+using NUnit.Framework;
+
+namespace IrregularVerbs.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        VolatileForm volatileForm;
+        Assert.Pass();
+        
+    }
+}
