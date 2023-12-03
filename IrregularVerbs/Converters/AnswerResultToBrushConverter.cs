f0e2139235e6038e1dbecf9a58d6c74fd39a94e8
JanZinch <jan.zinchanka@mail.ru>
2023-12-03 17:58:35 +0300

    feat: answer result

---
+++ IrregularVerbs/Converters/AnswerResultToBrushConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using IrregularVerbs.Models;
+
+namespace IrregularVerbs.Converters;
+
+public class AnswerResultToBrushConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is IrregularVerbAnswer.AnswerResult result)
+        {
+            switch (result)
+            {
+                case IrregularVerbAnswer.AnswerResult.Correct:
+                    return Brushes.LightGreen;
+                
+                case IrregularVerbAnswer.AnswerResult.Incorrect:
+                    return Brushes.LightCoral;
+                
+                case IrregularVerbAnswer.AnswerResult.None: 
+                default:
+                    return Brushes.WhiteSmoke;
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException("Value must be a System.Boolean");
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is SolidColorBrush brush)
+        {
+            return brush.Equals(Brushes.LightGreen);
+        }
+        else
+        {
+            throw new InvalidOperationException("Value must be a System.Windows.Media.SolidColorBrush");
+        }
+    }
+}
--- IrregularVerbs/Models/IrregularVerbAnswer.cs
+++ IrregularVerbs/Models/IrregularVerbAnswer.cs
@@ -9,18 +9,28 @@ public class IrregularVerbAnswer
     public string PastSimple { get; set; }
     public string PastParticiple { get; set; }
     
+    public AnswerResult Result { get; set; }
+    
     public IrregularVerbAnswer()
     {
         Term = string.Empty;
         Infinitive = string.Empty;
         PastSimple = string.Empty;
         PastParticiple = string.Empty;
+        Result = AnswerResult.None;
     }
 
     /*public static bool Inspect(IrregularVerbAnswer input, BaseIrregularVerb original)
     {
         
     }*/
+
+    public enum AnswerResult : byte
+    {
+        None = 0,
+        Correct = 1,
+        Incorrect = 2,
+    }
 
     public override string ToString()
     {
