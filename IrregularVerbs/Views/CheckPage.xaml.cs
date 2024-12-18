cf1f1aa040ea5b449e278da1f8901535fe995387
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2024-12-18 22:54:25 +0300

    feat: adjust grid

--- IrregularVerbs/Views/CheckPage.xaml.cs
+++ IrregularVerbs/Views/CheckPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using IrregularVerbs.ViewModels;
 using IrregularVerbs.Views.Base;
 
@@ -6,6 +8,9 @@ namespace IrregularVerbs.Views;
 
 public partial class CheckPage : EndPage
 {
+    private const double MinColumnWidthMultiplier = 0.75d;
+    private const double MaxColumnWidthMultiplier = 1.25d;
+    
     private readonly CheckPageViewModel _viewModel;
     
     public CheckPage(CheckPageViewModel viewModel)
@@ -18,6 +23,19 @@ public partial class CheckPage : EndPage
         RegisterBackCommand(_viewModel.BackCommand);
         _viewModel.OnTaskChecked += OnTaskChecked;
         Unloaded += OnUnloaded;
+    }
+    
+    private void AdjustGrid(object sender, RoutedEventArgs e)
+    {
+        foreach (DataGridColumn column in _grid.Columns)
+        {
+            double normalWidth = column.Width.Value;
+            
+            column.MinWidth = normalWidth * MinColumnWidthMultiplier;
+            column.MaxWidth = normalWidth * MaxColumnWidthMultiplier;
+        }
+
+        _grid.Columns.Last().CanUserResize = false;
     }
     
     private void OnTaskChecked()
